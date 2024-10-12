ae3b90d6ed0e177e3315025a272f9a39716c7ebb
Boxel <128452230+BoxelLogica@users.noreply.github.com>
2024-10-12 10:04:43 -0700

    Add files via upload

---
+++ default-buttons.js
@@ -0,0 +1,5 @@
+/* START Default Buttons */
+document.head.removeChild(document.head.children[12]);
+document.head.removeChild(document.head.children[13]);
+document.head.removeChild(document.head.children[14]);
+/* END Default Buttons */
